405391c92b9b595496e4437a7a6ee668d24d8e23
jpmv <jose.m.prieto.villar@accenture.com>
2020-11-09 16:09:12 +0100

    init project

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World\n");
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+res.send('Hello, World!\n');
+});
+app.get('/fool', function (req, res) {
+res.send('Hello, Fool!\n');
+});
+app.listen(8080, function () {
+console.log('Example app listening on port 8080!');
+});
+module.exports = app;
