e475321c6d576da12b28e8b67577da40e77881ce
jpmv <jose.m.prieto.villar@accenture.com>
2020-11-11 12:04:20 +0100

    add hi fool path

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
 res.send('Hello, World!\n');
 });
+
 app.get('/fool', function (req, res) {
 res.send('Hello, Fool!\n');
 });
+
+app.get('/hi', function (req, res) {
+res.send('Hi, Fool!\n');
+});
+
 app.listen(8080, function () {
 console.log('Example app listening on port 8080!');
 });
+
 module.exports = app;
